b6f7a7790f241ddc57da42450dcbe462a68f413f
Shubham Singh <suva068@gmail.com>
2022-09-04 03:57:10 +0530

    added message in sum.java

--- sum.java
+++ sum.java
@@ -2,7 +2,7 @@ class sum{
 public static void main(String[] args){
 int a=5;
 int b=7;
-System.out.print(a+b);
+System.out.print("sum = "a+b);
 
 }
 }
--- sum1.java
+++
@@ -1,8 +0,0 @@
-class sum{
-public static void main(String[] args){
-int a=5;
-int b=7;
-System.out.print("sum = "a+b);
-
-}
-}
